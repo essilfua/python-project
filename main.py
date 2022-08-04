eedb94833e4435662b9db1fad3d3ecc6108e9810
essilfua <109676721+essilfua@users.noreply.github.com>
2022-08-03 16:33:00 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+inputa =0
+inputb =0
+inputc =0
+
+
+inputa =int (input ("please enter the first input"))
+inputb =int(input ("please enter the second input"))
+inputc =inputa+ inputb
+print (inputc)
